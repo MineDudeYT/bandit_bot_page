96c1157c7fdd7cea1a0e8d7fdaa2c723406384ff
Unbright <71100622+MineDudeYT@users.noreply.github.com>
2024-05-09 17:17:38 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -38,21 +38,6 @@ const About = () => {
             Check out Commands &rarr;
           </Link>
         </p>
-
-        <section className="about-stats">
-          <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
-            <p className="p-color">Total Servers</p>
-          </div>
-          <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
-            <p className="p-color">Total Users</p>
-          </div>
-          <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
-            <p className="p-color">Total Commands Ran</p>
-          </div>
-        </section>
       </section>
     </>
   );
