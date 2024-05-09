8e974ba2ed41a30102b002207aab1612e90e6f54
Unbright <71100622+MineDudeYT@users.noreply.github.com>
2024-05-09 10:17:53 -0400

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -22,7 +22,7 @@ const Showcase = () => {
         <span className="p-color">Bot is online!</span>
         <div className="showcase-btn">
           <button className="btn">
-            <Link className="h-color no-decoration" href="#">
+            <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1137832494142587020&permissions=8&scope=bot">
               Invite now!
             </Link>
           </button>
