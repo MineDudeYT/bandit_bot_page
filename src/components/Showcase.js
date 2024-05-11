9bb2352d2ab59fee6ab3c831ff11e8da74977e2a
Unbright <71100622+MineDudeYT@users.noreply.github.com>
2024-05-11 14:54:29 -0400

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,16 +3,6 @@ import { motion } from "framer-motion";
 
 const Showcase = () => {
   return (
-    <script>
-function changePage(whereToGo, messageText)
-{
-    alert(messageText);
-    window.location=whereToGo;
-}
-</script>
-    document.getElementById('link').onclick = function () {
-    changePage("https://discord.com/oauth2/authorize?client_id=1137832494142587020&permissions=8&scope=bot", "If you attempt to add the bot to your server before purchasing access and talking to G0Osey21#0000 on Discord, the bot will instantly leave your server");
-};
     <motion.div
       initial={{
         opacity: 0,
@@ -30,7 +20,7 @@ function changePage(whereToGo, messageText)
         </p>
         <div className="showcase-btn">
           <button className="btn">
-            <Link id="link" className="h-color no-decoration">
+            <Link id="link" className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1137832494142587020&permissions=8&scope=bot" target="_blank">
               Invite now!
             </Link>
           </button>
