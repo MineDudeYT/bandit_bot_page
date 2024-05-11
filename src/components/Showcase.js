91c8be50bac517cdb4eb226d5725b41048f5954a
Unbright <71100622+MineDudeYT@users.noreply.github.com>
2024-05-11 15:08:42 -0400

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const Showcase = () => {
   const handleButtonClick = () => {
-    const confirmationMessage = "If you attempt to add the bot to your server before purchasing access and talking to G0Osey21#0000 on Discord, the bot will instantly leave your server.";
+    const confirmationMessage = "If you attempt to add the bot to your server before purchasing access and talking to G0Osey21#0000 on Discord, the bot will instantly leave your server. (You may also need to allow redirects for this to send you to Discord)";
     if (window.confirm(confirmationMessage)) {
       window.open('https://discord.com/oauth2/authorize?client_id=1137832494142587020&permissions=8&scope=bot', '_blank');
     }
