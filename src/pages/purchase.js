f066fec083f2046248e875e2b30e2ebea884f019
Unbright <71100622+MineDudeYT@users.noreply.github.com>
2024-05-12 13:37:19 -0400

    Update purchase.js

--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -19,97 +19,27 @@ export default function Purchase() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <section className="about-section">
-        <nav className="accordion arrows" id="commands">
-          <header className="box">
-            <label htmlFor="acc-close" className="box-title h-color">
-              Purchase
-            </label>
-          </header>
-          <input type="radio" name="accordion" id="cb1" />
-          <section className="box">
-            <label className="box-title p-color" htmlFor="cb1">
-              Minecraft 🎮
-            </label>
-            <label className="box-close" htmlFor="acc-close"></label>
-            <div className="box-content p-color">
-              <ul>
-                <li>
-                  <kbd>.m f who (faction name or player name)</kbd> -{" "}
-                  <span className="p-color">Displays faction information for specified faction or player.</span>
-                </li>
-                <li>
-                  <kbd>.m ftop (number - optional)</kbd> -{" "}
-                  <span className="p-color">Displays ftop information.</span>
-                </li>
-                <li>
-                  <kbd>.m pvptop (number - optional)</kbd> -{" "}
-                  <span className="p-color">Displays pvptop information.</span>
-                </li>
-                <li>
-                  <kbd>.m f list (number - optional)</kbd> -{" "}
-                  <span className="p-color">Displays f list information.</span>
-                </li>
-                <li>
-                  <kbd>.m f corner</kbd> -{" "}
-                  <span className="p-color">Displays world corner claim information.</span>
-                </li>
-                <li>
-                  <kbd>.outpost</kbd> -{" "}
-                  <span className="p-color">Displays outpost information.</span>
-                </li>
-                <li>
-                  <kbd>.seen (player name)</kbd> -{" "}
-                  <span className="p-color">Displays player last seen time.</span>
-                </li>
-              </ul>
-            </div>
-          </section>
-          <input type="radio" name="accordion" id="cb2" />
-          <section className="box">
-            <label className="box-title p-color" htmlFor="cb2">
-              Shardsearch 🗺️
-            </label>
-            <label className="box-close" htmlFor="acc-close"></label>
-            <div className="box-content p-color">
-              <ul>
-                <li>
-                  <kbd>.shardsearch</kbd> -{" "}
-                  <span className="p-color">Runs shardsearch for your set shard.</span>
-                </li>
-                <li>
-                  <kbd>.shardsearch setchannel</kbd> -{" "}
-                  <span className="p-color">Sets the channel for intervaled shardsearches (specific to your server). Must be ran in the channel you're setting it to. Only able to be used by server owners.</span>
-                </li>
-                <li>
-                  <kbd>.shardsearch setworld (shard name)</kbd> -{" "}
-                  <span className="p-color">Sets the shard for shardsearches (specific to your server). Must be ran in the channel you're setting it to. Only able to be used by server owners.</span>
-                </li>
-              </ul>
-            </div>
-          </section>
-          <input type="radio" name="accordion" id="cb3" />
-          <section className="box">
-            <label className="box-title p-color" htmlFor="cb3">
-              Moderation ⚙️
-            </label>
-            <label className="box-close" htmlFor="acc-close"></label>
-            <div className="box-content p-color">
-              <ul>
-                <li>
-                  <kbd>.whitelist adduser (user mention)</kbd> -{" "}
-                  <span className="p-color">Gives Discord user access to the bot's commands. Only able to be used by server owners.</span>
-                </li>
-                <li>
-                  <kbd>.whitelist removeuser (user mention)</kbd> -{" "}
-                  <span className="p-color">Removes Discord user access to the bot's commands. Only able to be used by server owners.</span>
-                </li>
-              </ul>
-            </div>
-          </section>
-          <input type="radio" name="accordion" id="acc-close" />
-        </nav>
-      </section>
+      <main className="showcase">
+        <h1 className="showcase-header h-color">Purchase</h1>
+        <h2 className="showcase-header-text h-color">
+          There are 2 ways to purchase the bot. A one-time purchase of $100 or a base fee of $50 and a monthly subscription of $5. Both ways get you full access to the bot and all of it's commands, but give you a freedom of either going all in or just testing it out. Either are both great options.
+        </h2>
+
+        <span className="p-color">Bot is online!</span>
+        <div className="showcase-btn">
+          <button className="btn">
+            <Link className="h-color no-decoration" href="#">
+              Invite now!
+            </Link>
+          </button>
+
+          <button className="btn">
+            <Link className="h-color no-decoration" href="#">
+              Join server!
+            </Link>
+          </button>
+        </div>
+      </main>
       <Footer />
     </motion.div>
   );
